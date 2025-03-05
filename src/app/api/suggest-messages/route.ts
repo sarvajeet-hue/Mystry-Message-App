04a2643a50296dbf140cdd639f44b16d61a2c469
sarvajeet-hue <sarvajeet.1923en1095@kiet.edu>
2025-03-05 16:23:29 +0530

    AI Integration successfully

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,7 +1,8 @@
-import { openai } from "@ai-sdk/openai";
+
 import { streamText } from "ai";
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
+const openai = new OpenAI();
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -11,13 +12,32 @@ export async function POST(req: Request) {
     const prompt =
       "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
-    const result = streamText({
-      model: openai("gpt-4o"),
-      prompt,
-    });
+    // const result = streamText({
+    //   model: openai("gpt-4o"),
+    //   prompt,
+    // });
+    const completion = await openai.chat.completions.create({
+      model: "gpt-4o",
+      messages: [
+          { role: "developer", content: "You are a helpful assistant." },
+          {
+              role: "user",
+              content: prompt,
+          },
+      ],
+      store: true,
+  });
 
-    return result.toDataStreamResponse();
-  } catch (error) {
+    let result = completion.choices[0].message 
+    // const final_result =  result.toDataStreamResponse();
+    console.log(completion.choices[0].message);
+    return Response.json({
+      success : true, 
+      message : "Hit successfully", 
+      result
+    })
+    
+  } catch (error) { 
     if (error instanceof OpenAI.APIError) {
       const { name, status, headers, message } = error;
       return NextResponse.json(
@@ -27,7 +47,7 @@ export async function POST(req: Request) {
           headers,
           message,
         },
-        { status }
+        { status  }
       );
     } else {
       console.error("An unexpected error occured");
