42622532ef74e1c1e77b78d77c49bcc62d5ed0e7
sarvajeet-hue <sarvajeet.1923en1095@kiet.edu>
2025-03-20 18:39:53 +0530

    Implementing the signIn form with zod

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,21 +1,85 @@
 "use client";
 
-import { FormField, FormItem, FormLabel } from "@/components/ui/form";
-import { Form, FormProvider } from "react-hook-form";
-import {useForm} from 'react-hook-form'
-export default function page() {
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { signInSchema } from "@/schemas/signInSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
+export default function SignIn() {
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+  });
+  
+
+  const onSubmit = async (
+    data: z.infer<
+      typeof signInSchema
+    > /*  {username : string , password : string}*/
+  ) => {
+
+
+  };
 
   return (
-    <div>
-        <form >
+    <div className="flex justify-center items-center min-h-screen bg-gray-100 p-3">
+      <div className="w-full max-w-sm sm:max-w-sm md:max-w-md lg:max-w-lg p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Verify Your Account
+          </h1>
+          <p className="mb-4">Enter the verification code sent to your email</p>
+        </div>
 
-              <label> 
-                Email :
-                <input type="email" placeholder="email" />
-              </label>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Username" {...field} />
+                  </FormControl>
 
-        </form>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="password" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+
+        
+      </div>
     </div>
   );
 }
