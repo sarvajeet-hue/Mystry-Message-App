00cd4a19915f4a02b63de8b1aa64e86122f557c3
sarvajeet-hue <sarvajeet.1923en1095@kiet.edu>
2025-04-02 18:24:59 +0530

    prod update

--- next.config.ts
+++ next.config.ts
@@ -8,3 +8,20 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Change this to your frontend domain in production
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+        ],
+      },
+    ];
+  },
+};
--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -37,8 +37,6 @@ export default function SignIn() {
       identifier: data.identifier,
       password: data.password,
       callbackUrl: "/dashboard",
-      
-
     });
     console.log("result", result);
 
